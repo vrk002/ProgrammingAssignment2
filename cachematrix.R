5f4a06183ecce164641edcd5d09e9433309add5d
Venu Kottapally <vrk002@users.noreply.github.com>
2015-03-22 15:42:57 -0700

    Update cachematrix.R
    Implemented the assignment requirements

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##The below function takes a matrix as input, which is assumed to be inversible
+## Then the below funciton returns a list with 4 funcitons
+## 1. set: that sets the matrix to new values
+## 2. get: returns the values of the matrix
+## 3. setinvmatrix: this sets the inverse of the matrix
+## 4. getinvmatrix: returns the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invm <- NULL
+  
+  set <- function(y){
+    x <<- y
+    invm <<- NULL
+  }
+  
+  get <- function() x
+  
+  setinvmatrix <- function(new_inv) invm <<- new_inv
+  getinvmatrix<-function() invm
+  
+  list(set = set, 
+       get = get,
+       setinvmatrix = setinvmatrix,
+       getinvmatrix = getinvmatrix)
 }
 
 
-## Write a short comment describing this function
+## The below function first checks if the inverse of the matrix is already calculated
+## if the inverse is calculated, then returns it
+## if not, then calcualtes the inverse of the matrix and returns it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## check if the matrix has inverse of it calculated
+  invm <- x$getinvmatrix()
+  
+  if(!is.null(invm)){
+    message("getting cached data")
+    return(invm)
+  }
+  temp_mat <- x$get()
+  invm <- solve(temp_mat)
+  
+  x$setinvmatrix(invm)
+  
+  invm
 }
